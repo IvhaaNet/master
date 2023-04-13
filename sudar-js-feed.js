a42a6300cb703400af39b14f476657160d9633e5
IvhaaNet <35192388+IvhaaNet@users.noreply.github.com>
2023-04-14 05:05:16 +0700

    Create sudar-js-feed.js

---
+++ sudar-js-feed.js
@@ -0,0 +1,21 @@
+var multiFeed = {
+    feedsUri: [
+        {
+            name: "Blogger",
+            url: "https://www.sudarjurnalblog.web.id",
+            tag: ""
+        }
+    ],
+    numPost: 4,
+    showThumbnail: true,
+    showSummary: false,
+    summaryLength: 80,
+    titleLength: "auto",
+    thumbSize: 72,
+    newTabLink: true,
+    containerId: "feed-list-container",
+    readMore: {
+        text: "Selengkapnya",
+        endParam: "?max-results=8"
+    }
+};
